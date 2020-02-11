f570345d32482484da0734120374698e372fcd10
Aidan Denlinger <adenling@ucsd.edu>
2020-02-11 13:39:35 -0800

    updated Test.hs to have hardcoded file paths for demo

--- tests/Test.hs
+++ tests/Test.hs
@@ -13,9 +13,6 @@ import qualified Data.ByteString.Lazy          as LBS
 
 import qualified Language.Wasm                 as Wasm
 import qualified Language.Wasm.Script          as Script
-import qualified Language.Wasm.Lexer           as Lexer
-import qualified Language.Wasm.Parser          as Parser
-import qualified Language.Wasm.Binary          as Binary
 
 main :: IO ()
 main = do
@@ -30,10 +27,24 @@ main = do
         script
   defaultMain $ testGroup "Wasm Core Test Suit" scriptTestCases
 
-toBinary :: String -> IO ()
-toBinary input = do
+-- | Basic function to take in filepaths to WasmText input and output binary
+toBinary :: String -> String -> IO ()
+toBinary input output = do
+  -- read the text file into a lazy bytestring buffer
   content <- LBS.readFile input
-  case Lexer.scanner content >>= Parser.parseModule of
+  -- parse the text, which returns Either String (error) Wasm.Module
+  case Wasm.parse content of
+    -- When it's a Wasm.Module, encode the module into binary and output
+    Right mod    -> LBS.writeFile output $ Wasm.encodeLazy mod
+    -- Otherwise, print error string
+    Left  reason -> putStrLn reason
+
+
+-- | Hardcoded toBinary with exact filepaths so I don't screw that up
+toBinaryHC :: IO ()
+toBinaryHC = do
+  content <- LBS.readFile "/home/aidan/Desktop/testing/test.wat"
+  case Wasm.parse content of
     Right mod ->
-      LBS.writeFile "/home/aidan/Desktop/bin.wasm" $ Binary.dumpModuleLazy mod
+      LBS.writeFile "/home/aidan/Desktop/testing/bin.wasm" $ Wasm.encodeLazy mod
     Left reason -> putStrLn reason
