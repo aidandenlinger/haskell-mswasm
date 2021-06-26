6a9e6dd94befb86bdac2d663c3ad2f9edfed0da6
Aidan Denlinger <adenling@ucsd.edu>
2021-06-25 21:48:55 -0700

    remove showModule since it relied on conversion code

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,30 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main
-  ( main
+  ( main,
   )
 where
 
-import           Test.Tasty
-import           Test.Tasty.HUnit
-
-import qualified System.Directory              as Directory
-
-import qualified Data.ByteString.Lazy          as LBS
-
-import qualified Language.Wasm                 as Wasm
-import qualified Language.Wasm.Script          as Script
+import qualified Data.ByteString.Lazy as LBS
+import qualified Language.Wasm as Wasm
+import qualified Language.Wasm.Script as Script
+import qualified System.Directory as Directory
+import Test.Tasty
+import Test.Tasty.HUnit
 
 main :: IO ()
 main = do
-  files           <- Directory.listDirectory "tests/samples"
+  files <- Directory.listDirectory "tests/samples"
   scriptTestCases <- (`mapM` files) $ \file -> do
     Right script <- Wasm.parseScript <$> LBS.readFile ("tests/samples/" ++ file)
-    return $ testCase file $ do
-      Script.runScript
-        (\msg assert -> assertFailure
-          ("Failed assert: " ++ msg ++ ". Assert " ++ show assert)
-        )
-        script
+    return $
+      testCase file $ do
+        Script.runScript
+          ( \msg assert ->
+              assertFailure
+                ("Failed assert: " ++ msg ++ ". Assert " ++ show assert)
+          )
+          script
   defaultMain $ testGroup "Wasm Core Test Suit" scriptTestCases
 
 -- | Basic function to take in filepaths to WasmText input and output binary
@@ -37,9 +37,9 @@ toBinary input output = do
   -- parse the text, which returns Either String (error) Wasm.Module
   case Wasm.parse content of
     -- When it's a Wasm.Module, encode the module into binary and output
-    Right mod    -> LBS.writeFile output $ Wasm.encodeLazy mod
+    Right mod -> LBS.writeFile output $ Wasm.encodeLazy mod
     -- Otherwise, print error string
-    Left  reason -> putStrLn reason
+    Left reason -> putStrLn reason
 
 -- | Hardcoded toBinary with exact filepaths, run from root dir
 toBinaryHC :: IO ()
@@ -61,11 +61,11 @@ showFullModule :: String -> IO ()
 showFullModule input = do
   binary <- LBS.readFile input
   case Wasm.decodeLazy binary of
-    Right mod    -> print mod
-    Left  reason -> putStrLn reason
+    Right mod -> print mod
+    Left reason -> putStrLn reason
 
-showModule :: String -> IO ()
-showModule s = Wasm.showModuleFromFile s >>= putStrLn
+-- showModule :: String -> IO ()
+-- showModule s = Wasm.showModuleFromFile s >>= putStrLn
 
 -- | Hardcoded toModule to examine the binary bin.wasm in toBinaryTest
 toModuleHC :: IO ()
